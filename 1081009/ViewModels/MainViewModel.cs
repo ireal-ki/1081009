991f20f304ba7fff59bf2a18f15408bb15d20ed5
katopz <katopz@gmail.com>
2014-06-25 22:12:09 -0700

    add more sample

--- 1081009/ViewModels/MainViewModel.cs
+++ 1081009/ViewModels/MainViewModel.cs
@@ -66,7 +66,7 @@ namespace _1081009.ViewModels
                 ID = "0",
                 AItem = "A",
                 AItemName = "Apple",
-                ImageURI = "http://1081009.tourismthailand.org/oldweb/uploads/trips/n/203784.jpg",
+                ImageURI = "http://1081009.tourismthailand.org/oldweb/uploads/trips/n/203811.jpg",
                 ViewTotalNums = 1000,
                 LikeTotalNums = 20,
                 IsLike = false,
@@ -77,7 +77,7 @@ namespace _1081009.ViewModels
                 ID = "1",
                 AItem = "B",
                 AItemName = "Banana",
-                ImageURI = "http://1081009.tourismthailand.org/oldweb/uploads/trips/n/203783.jpg",
+                ImageURI = "http://1081009.tourismthailand.org/oldweb/uploads/trips/n/203810.jpg",
                 ViewTotalNums = 2000,
                 LikeTotalNums = 21,
                 IsLike = true,
@@ -88,10 +88,32 @@ namespace _1081009.ViewModels
                 ID = "2",
                 AItem = "C",
                 AItemName = "Carrot",
-                ImageURI = "http://1081009.tourismthailand.org/oldweb/uploads/trips/n/203783.jpg",
+                ImageURI = "http://1081009.tourismthailand.org/oldweb/uploads/trips/n/203808.jpg",
                 ViewTotalNums = 3000,
                 LikeTotalNums = 22,
                 IsLike = false,
+                IsCompleted = false
+            });
+            this.Items.Add(new ItemViewModel()
+            {
+                ID = "1",
+                AItem = "B",
+                AItemName = "Banana",
+                ImageURI = "http://1081009.tourismthailand.org/oldweb/uploads/trips/n/203807.jpg",
+                ViewTotalNums = 4000,
+                LikeTotalNums = 23,
+                IsLike = true,
+                IsCompleted = false
+            });
+            this.Items.Add(new ItemViewModel()
+            {
+                ID = "2",
+                AItem = "C",
+                AItemName = "Carrot",
+                ImageURI = "http://1081009.tourismthailand.org/oldweb/uploads/trips/n/203806.jpg",
+                ViewTotalNums = 4000,
+                LikeTotalNums = 24,
+                IsLike = true,
                 IsCompleted = false
             });
             this.IsDataLoaded = true;
